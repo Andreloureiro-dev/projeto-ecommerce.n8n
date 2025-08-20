37bd8d1164df4f63d0b65e743c0533bc615d79c4
André Loureiro <andrexprofissional@gmail.com>
2025-08-20 20:33:28 +0100

    funcionalidade para abrir e fechar o menu hamburguer em encrãs mais pequenos

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const menuHamburguer = document.querySelector(".menu-hamburguer");
+    const header = document.querySelector(".header");
+
+    menuHamburguer.addEventListener("click", () => {
+        header.classList.toggle("menu-ativo");
+    });
+});
